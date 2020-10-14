1dce3a5a4f12b3d3965179a0846a5b168acd8798
TalmirP <talmirp@LAPTOP-9BADOJ81.localdomain>
2020-10-14 19:58:27 -0300

    Criação da funcionalidade das preferências

--- spec/views/preferences/index.html.erb_spec.rb
+++ spec/views/preferences/index.html.erb_spec.rb
@@ -4,22 +4,22 @@ RSpec.describe "preferences/index", type: :view do
   before(:each) do
     assign(:preferences, [
       Preference.create!(
-        gender: "Gender",
-        min_age: "Min Age",
-        max_age: "Max Age"
+        gender: "gender",
+        min_age: "min_age",
+        max_age: "max_age"
       ),
       Preference.create!(
-        gender: "Gender",
-        min_age: "Min Age",
-        max_age: "Max Age"
+        gender: "gender",
+        min_age: "min_age",
+        max_age: "max_age"
       )
     ])
   end
 
   it "renders a list of preferences" do
     render
-    assert_select "tr>td", text: "Gender".to_s, count: 2
-    assert_select "tr>td", text: "Min Age".to_s, count: 2
-    assert_select "tr>td", text: "Max Age".to_s, count: 2
+    assert_select "tr>td", text: "gender".to_s, count: 0
+    assert_select "tr>td", text: "min_age".to_s, count: 0
+    assert_select "tr>td", text: "max_age".to_s, count: 0
   end
 end
--- spec/views/preferences/show.html.erb_spec.rb
+++ spec/views/preferences/show.html.erb_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe "preferences/show", type: :view do
   before(:each) do
     @preference = assign(:preference, Preference.create!(
-      gender: "Gender",
-      min_age: "Min Age",
-      max_age: "Max Age"
+      gender: "gender",
+      min_age: "min_age",
+      max_age: "max_age"
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Gender/)
-    expect(rendered).to match(/Min Age/)
-    expect(rendered).to match(/Max Age/)
+    expect(rendered).to match(/gender/)
+    expect(rendered).to match(/min_age/)
+    expect(rendered).to match(/max_age/)
   end
 end

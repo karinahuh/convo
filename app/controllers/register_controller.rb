c6a1c0e2bc64ee269a77b555fe38811558fa2b79
thlasso <thaislasso@gmail.com>
2020-10-14 22:33:36 -0300

    fix: register controller sintax errors

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,12 +1,15 @@
 class RegisterController < ApplicationController
 	def create
 		@user = Register.new(user_params)
-		
-		if @user.save 
-			redirect-to :action 'index', id: @user.id notice: 'Seja bem Vind@!'
-		else 
-			render action: 'new'
-
+	        
+                respond_to do |format|
+		   if @user.save 
+                        format.html { redirect-to  :action 'index', id: @user.id notice: 'Seja bem Vind@!' }
+                        format.json { render :show, status: :created, location: @user }
+                   else
+                     format.html { render action: 'new' }
+                     format.json { render json: @user.errors, status: :unprocessable_entity }
+                   end
                 end
         end
 end

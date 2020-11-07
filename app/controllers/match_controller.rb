ac95d48d18bba265b81e8113f8df14147194e4f0
karinahuh <karinahuh@DESKTOP-7B6E2JO.localdomain>
2020-11-07 18:17:17 -0300

    iniciando algoritmo de match

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,2 +1,17 @@
 class MatchController < ApplicationController
+    def match(preference_params)
+        @usuario = User.where(gender: preference_params[gender]).and(User.where(min_age: preference_params[min_age])).and(User.where(max_age: preference_params[max_age]))
+        respond_to do |format|
+            if(@usuario)
+                redirect_to chat_path
+            else
+                render js: "alert('Não foi possível encontrar um match para você :( ');"
+            end
+        end 
+    end
+
+    private
+    def preference_params
+       params.require(:preference).permit(:gender, :min_age, :max_age)
+   end
 end
--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -60,7 +60,7 @@ class PreferencesController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_preference
